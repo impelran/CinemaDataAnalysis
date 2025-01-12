3115fbfb126b141cf283f6777867c7f8efa2152c
impelran <chafer.height4285@eagereverest.com>
2025-01-12 14:30:22 +0100

    Exercise 5

---
+++ Exercise5.py
@@ -0,0 +1,17 @@
+current_screens = 2
+current_seats = 120
+current_population = 20000
+
+# We can use the number of screens to predict the number of entries by using the graph from exercise 3.
+## With 2 screens, we can expect around 600,000 entries.
+entries_screen = 600000
+
+# We can use the number of seats to predict the number of entries by using the graph from exercise 3.
+## With 120 seats, we can expect around 1,000 entries.
+entries_seats = 1000
+
+print("Le nombre d'entrées approximatif avec 2 ecrans: ", entries_screen)
+print("Le nombre d'entrées approximatif avec 120 fauteuils: ", entries_seats)
+
+print("Il est inutile de calculer le nombre d'entrées avec la population actuelle, car il est impossible de prédire le nombre d'entrées avec la population actuelle.")
+print("Cependant, il est possible d'affirmer que le nombre d'entrées sera plus éleve avec une augmentation du nombre de fauteuils grace au résultat de l'exercice 3.")
